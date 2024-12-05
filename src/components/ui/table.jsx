e77b6807896badaa128b2966cee2978aec7e0982
ndizeye <ndiyizeye123@gmail.com>
2024-12-05 23:24:04 +0200

    asa

--- src/components/ui/table.jsx
+++ src/components/ui/table.jsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { cn } from "../../lib/utils"
 import { ChevronLeft, ChevronRight } from "lucide-react"
-import { Button } from "./button"
+import { Button } from "../ui/Button.jsx"
 
 const Table = React.forwardRef(({ className, ...props }, ref) => (
   <div className="relative w-full overflow-auto">
