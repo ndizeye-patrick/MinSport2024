0984f20af8172c5421d97143be817497c577fcf8
ndizeye <ndiyizeye123@gmail.com>
2024-12-05 22:53:27 +0200

    #001

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,54 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+// eslint.config.js
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+export default [
+    {
+        files: ['**/*.{js,jsx}'],
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.es2021
+            },
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+                ecmaFeatures: {
+                    jsx: true
+                }
+            }
+        },
+        plugins: {
+            react,
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh
+        },
+        rules: {
+            // React plugin rules
+            'react/react-in-jsx-scope': 'off', // Not needed in modern React
+            'react/prop-types': 'warn',
+            'react/no-unescaped-entities': 'warn',
+
+            // React Hooks rules
+            'react-hooks/rules-of-hooks': 'error',
+            'react-hooks/exhaustive-deps': 'warn',
+
+            // React Refresh plugin rules
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true }
+            ],
+
+            // General ESLint rules
+            'no-unused-vars': ['warn', {
+                args: 'after-used',
+                ignoreRestSiblings: true
+            }],
+            'no-console': 'warn',
+            'eqeqeq': 'error',
+            'no-duplicate-imports': 'error'
+        }
+    }
+];
